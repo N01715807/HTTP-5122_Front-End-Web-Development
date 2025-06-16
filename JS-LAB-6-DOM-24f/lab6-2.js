b8893cc0a83d025a46f67751964335e06582e5af
pengchengli <n01715807@humber.ca>
2025-06-15 21:54:16 -0400

    HTTP-5122_Front-End Web Development

--- JS-LAB-6-DOM-24f/lab6-2.js
+++ JS-LAB-6-DOM-24f/lab6-2.js
@@ -3,20 +3,33 @@
 
 //LISTEN FOR load EVENT
 
-
 //'WRAPPER' FUNCTION FOR DOM LOGIC
-
+window.onload = pageReady;
+function pageReady(){
 	//GET DOM ELEMENTS WE'LL NEED
-
+var MYSTERYBOX = document.getElementById("mysteryBox");
+var buttonBox  = document.getElementById("buttonBox");
 
 	//====CREATE THE FUNCTIONS WE'LL NEED====
 	//FUNCTION TO ASK USER
+MYSTERYBOX.onmouseover = switchMYSTERYBOX;
 
-
+function switchMYSTERYBOX() {
+       if(confirm('Do you want to see something?')){
+		MYSTERYBOX.style.display = 'none';
+		buttonBox.style.display  = 'block';} ;
+    }
+	
 	//FUNCTION TO CHANGE buttonBox
+buttonBox.onclick = buttonBoxonclick;
 
-
+function buttonBoxonclick(){
+	buttonBox.style.width = "600px";
+    buttonBox.style.backgroundColor = "orange";
+    buttonBox.textContent = "SURPRISE!!";
+}
 	//SETUP LISTENERS
 
 
-//END onload FUNCTION
+
+}//END onload FUNCTION
