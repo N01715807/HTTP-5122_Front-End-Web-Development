352ec8263d94ddcf094909f8d06fb9d59a24596d
pengchengli <n01715807@humber.ca>
2025-06-17 10:04:56 -0400

    HTTP-5122_Front-End Web Development

---
+++ JS-LAB-7-FormValidation25w/Lab-7-forms.js
@@ -0,0 +1,14 @@
+/* LAB 7 - SHIPPING FORM */
+//Order Shipping object (for extra EXTRA challenge, otherwise, ignore it)
+var shipInfo = {
+	name: "",
+	pc: "",
+	speed: "",
+	cost: 0
+};
+
+//==== CREATE YOUR PSEUDOCODE COMMENTS FIRST
+
+//WAIT FOR THE PAGE TO LOAD
+
+
