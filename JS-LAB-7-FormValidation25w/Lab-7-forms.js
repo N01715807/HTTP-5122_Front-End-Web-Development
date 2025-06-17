118f9cca6f45a613237015615fae1db2d65db024
pengchengli <n01715807@humber.ca>
2025-06-17 12:41:42 -0400

    HTTP-5122_Front-End Web Development

--- JS-LAB-7-FormValidation25w/Lab-7-forms.js
+++ JS-LAB-7-FormValidation25w/Lab-7-forms.js
@@ -10,5 +10,54 @@ var shipInfo = {
 //==== CREATE YOUR PSEUDOCODE COMMENTS FIRST
 
 //WAIT FOR THE PAGE TO LOAD
+window.onload = onReady;
 
+function onReady() {
+	var formHandle   = document.getElementById("shippingForm");
+    var inputName    = document.getElementById("in_Name");
+    var inputPC      = document.getElementById("in_pc");
+	var selectSpeed  = document.getElementById("in_Speed");
 
+    var thanksBlock  = document.getElementById("thanks_msg");
+    var spanName     = document.getElementById("thanksCustomer");
+    var spanPC       = document.getElementById("thanksPC");
+	var spanSpeed   = document.getElementById("thanksSpeed");
+    var spanCost    = document.getElementById("thanksCost");
+
+	formHandle.onsubmit = processForm;
+
+	function processForm(evt) {
+
+	evt.preventDefault();
+    var Valid = true;
+
+    if (inputName.value.trim() === "") {
+      inputName.style.backgroundColor = "lightcoral";
+      inputName.focus();
+      Valid = false;
+    }
+    else if (inputPC.value.trim() === "") { 
+      inputPC.style.backgroundColor = "lightcoral";
+      inputPC.focus();
+      Valid = false;
+    }
+	else if (selectSpeed.value === "0" || selectSpeed.value === "") {
+      selectSpeed.style.backgroundColor = "lightcoral";
+      selectSpeed.focus();
+      valid = false;
+    }
+
+    if (!Valid) { return false; }
+
+    spanName.textContent = inputName.value.trim();
+    spanPC.textContent = inputPC.value.trim();
+	spanSpeed.textContent = selectSpeed.options[selectSpeed.selectedIndex].textContent;
+    spanCost.textContent  = Number(selectSpeed.value).toFixed(2);
+
+    formHandle.style.display = "none";
+    thanksBlock.style.display = "block";
+
+    return false;
+  }
+
+}
