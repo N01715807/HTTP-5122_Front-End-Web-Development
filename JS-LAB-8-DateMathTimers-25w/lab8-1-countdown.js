08146b0181fbd7127879d507f11778b6d7fa9176
pengchengli <n01715807@humber.ca>
2025-07-08 13:56:31 -0400

    HTTP-5122_Front-End Web Development

---
+++ JS-LAB-8-DateMathTimers-25w/lab8-1-countdown.js
@@ -0,0 +1,39 @@
+﻿/* LAB 8-1 - FINAL COUNTDOWN!! */
+window.onload = function()
+{
+	let now = new Date()
+	let dueDate = new Date("August 15, 2025 18:00:00")
+
+	let todayData = document.getElementById("todayData")
+	todayData.textContent = now.toDateString()
+	let finalData = document.getElementById("finalData")
+	finalData.textContent = dueDate.toDateString()
+
+	let dueData = document.getElementById("dueData")
+	let diff = (dueDate.getTime() - now.getTime())/ 86400000
+
+	let leftDate = Math.floor(diff)
+
+	if( leftDate <0 ){
+		let countMsg = document.getElementById("countMsg")
+		countMsg.textContent = "it is OVER" 
+	}else {
+		dueData.textContent = leftDate;
+  }
+}
+
+//create page load listener
+
+//create page load function
+
+	//create variables for required HTML elements
+	
+	//create variables for now date and due date
+	
+	//OUTPUT NOW DATE & DUE DATE TO HTML PAGE
+	
+	//CONVERT TO UTC AND SUBTRACT TO GET TIME DIFFERENCE
+	
+	//CONVERT TIME DIFFERENCE TO WHOLE NUMBER OF DAYS
+		
+	//LOGIC TO CHECK IF DUE DATE HAS PASSED, AND OUPUT APPROPRIATE MESSAGE TO HTML PAGE
---
+++ JS-LAB-8-DateMathTimers-25w/lab8-2-clock.js
@@ -0,0 +1,62 @@
+﻿/* LAB 8.2 - STOP TIME */
+window.onload = function(){
+
+timeDisplay();
+
+document.getElementById("btnStart").addEventListener("click", startClock);
+document.getElementById("btnStop").addEventListener("click", stopClock);
+
+}
+
+let runingTime =null
+
+function format(num){
+	if (num < 10) {
+		return "0" + num;
+	} else {
+		return num;
+	}
+}
+
+function timeDisplay(){
+	let now = new Date();
+	let hoursOut = document.getElementById("hoursOut")
+	hoursOut.textContent = `${format(now.getHours())}`
+
+    let minsOut = document.getElementById("minsOut")
+    minsOut.textContent = `:${format(now.getMinutes())}`
+
+    let secsOut = document.getElementById("secsOut")
+    secsOut.textContent = `:${format(now.getSeconds())}`
+}
+
+function startClock(){
+	if(!runingTime){timeDisplay()
+		runingTime = setInterval(timeDisplay, 1000)
+	}
+	}
+
+function stopClock(){
+	if(runingTime){clearInterval(runingTime)
+		runingTime = null}
+}
+
+//create page load listener
+
+//create page load function
+
+	//create variables for required HTML elements
+	
+	//create time variable so all functions have access to it
+	
+	
+	//CREATE FUNCTION THAT DISPLAYS THE TIME
+	
+	
+	//CREATE FUNCTION TO START THE CLOCK.
+	
+	
+	//CREATE FUNCTION TO STOP THE CLOCK
+	
+	
+	// SET EVENT LISTENERS
