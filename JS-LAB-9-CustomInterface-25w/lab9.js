a4e2c375f7acdc73d1bc8b5a13f8d17bac127976
pengchengli <n01715807@humber.ca>
2025-07-15 11:01:47 -0400

    HTTP-5122_Front-End Web Development

---
+++ JS-LAB-9-CustomInterface-25w/lab9.js
@@ -0,0 +1,50 @@
+﻿//LAB 9-DATA STORAGE: INDEX PAGE
+//window.onload
+window.onload = function(){
+
+    //check for stored values
+        //retrieve stored values
+        let storedName = localStorage.getItem("name");
+        let storedColor = localStorage.getItem("colour");
+        //change welcome text to stored name
+        if (storedName && storedColor) {
+            //change BG colour to stored colour
+            document.getElementById("newMsgBox").textContent = `Welcome, ${storedName}`;
+            //  document.body.style.background = 
+            document.body.style.background = storedColor;
+        }};
+
+        	
+//#####============== DO THIS PART FIRST! ===============		
+    //get the form and set submit listener
+	 document.forms["infoForm"].addEventListener("submit", function (e) {
+        e.preventDefault();
+        let nameInput = document.getElementById("inName").value;
+        let colorInput = document.getElementById("inColor").value;
+        localStorage.setItem("name", nameInput);
+        localStorage.setItem("colour", colorInput);
+        location.reload();
+    });
+
+    document.getElementById("btnDel").addEventListener("click", function () {
+        localStorage.removeItem("name");
+        localStorage.removeItem("colour");
+        location.reload();
+    });
+	
+
+	//onsubmit Function
+        
+        //get values from form
+		
+		
+		//console.log the form values
+
+        
+        //store values
+        
+        
+
+
+
+    
---
+++ JS-LAB-9-CustomInterface-25w/products.js
@@ -0,0 +1,10 @@
+﻿//LAB 9-DATA STORAGE: PRODUCTS PAGE
+window.onload = function () {
+    let storedName = localStorage.getItem("name");
+    let storedColor = localStorage.getItem("colour");
+
+    if (storedName && storedColor) {
+        document.getElementById("newMsgBox").textContent = `Welcome, ${storedName}`;
+        document.body.style.background = storedColor;
+    }
+};
