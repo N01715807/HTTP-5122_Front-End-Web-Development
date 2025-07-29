f5bcf260cfa390fee1f3ffc8aa0cbe63fd416b40
pengchengli <n01715807@humber.ca>
2025-07-29 10:29:10 -0400

    HTTP-5122_Front-End Web Development

--- JS-LAB-11-jQUERY-24f/lab10-1-jquery.js
+++ JS-LAB-11-jQUERY-24f/lab10-1-jquery.js
@@ -1,23 +1,22 @@
 //LAB 10 - 1 FAQ PAGE
 
 //Listen for window load the jQuery way
-
-
-
+$(document).ready(function () {
 	//Inside of here is your jQuery/JavaScript
-
-
+	$(".contentBox").hide();
 	//ADD CLICK EVENT TO <h2>
-	
-	
-	
-	
-	
-	
+	$("h2").click(function () {
+    $(".contentBox").not($(this).next()).slideUp();
+    $(this).next(".contentBox").slideToggle(3000); 
+  });
 	//CHANGE <p> BACKGROUND ON HOVER
-
-
-
-
-
+	$(".contentBox").hover(
+    function () {
+      $(this).addClass("textHovered");
+    },
+    function () {
+      $(this).removeClass("textHovered");
+    }
+  );
+});
 
--- JS-LAB-11-jQUERY-24f/lab10-2-jquery.js
+++ JS-LAB-11-jQUERY-24f/lab10-2-jquery.js
@@ -1,18 +1,18 @@
 //LAB 10 - 2 INVENTORY PAGE
-
-
-
-
-
-
-
-
+$(document).ready(function () {
+	$(".desc").hide();
 	//ADD MOUSEOVER/MOUSEOUT FUNCTIONS FOR <tr>
-
-
-
-
-
-
-
+	$("#tblInventory tbody tr").hover(
+    function () {
+      $(this).addClass("selected");
+    },
+    function () {
+      $(this).removeClass("selected");
+    }
+  );
 	//ADD CLICK EVENT TO <tr>
+	$("#tblInventory tbody tr").click(function () {
+		$(".desc").hide();
+		$(this).find(".desc").show();
+	});
+});
